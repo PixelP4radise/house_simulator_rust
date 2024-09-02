ea2bae96a46f1cd42db4b37c69d4900c3bf73aff
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2024-09-02 16:02:26 +0100

    specified type for each Property

--- src/app/house/room/property.rs
+++ src/app/house/room/property.rs
@@ -1,7 +1,6 @@
 pub trait Property {
     type Output;
     fn get_value(&self) -> Self::Output;
-    fn get_descrition(&self) -> &str;
 }
 
 //os sensores e aparelhos devem comunicar com as propriedades através das suas chaves
@@ -11,45 +10,87 @@ pub struct Temperature {
     value: u16,
 }
 
-impl Property for Temperature {}
+impl Property for Temperature {
+    type Output = u16;
+
+    fn get_value(&self) -> Self::Output {
+        self.value
+    }
+}
 
 #[derive(Default)]
 pub struct Light {
     value: u16,
 }
 
-impl Property for Light {}
+impl Property for Light {
+    type Output = u16;
+
+    fn get_value(&self) -> Self::Output {
+        self.value
+    }
+}
 
 #[derive(Default)]
 pub struct Radiation {
     value: u16,
 }
 
-impl Property for Radiation {}
+impl Property for Radiation {
+    type Output = u16;
+
+    fn get_value(&self) -> Self::Output {
+        self.value
+    }
+}
 
 #[derive(Default)]
 pub struct Vibration {
     value: u16,
 }
 
-impl Property for Vibration {}
+impl Property for Vibration {
+    type Output = u16;
+
+    fn get_value(&self) -> Self::Output {
+        self.value
+    }
+}
 
 #[derive(Default)]
 pub struct Humidity {
     value: u16,
 }
 
-impl Property for Humidity {}
+impl Property for Humidity {
+    type Output = u16;
+
+    fn get_value(&self) -> Self::Output {
+        self.value
+    }
+}
 
 #[derive(Default)]
 pub struct Smoke {
     value: u16,
 }
 
-impl Property for Smoke {}
+impl Property for Smoke {
+    type Output = u16;
+
+    fn get_value(&self) -> Self::Output {
+        self.value
+    }
+}
 
 #[derive(Default)]
 pub struct Sound {
     value: u16,
 }
-impl Property for Sound {}
+impl Property for Sound {
+    type Output = u16;
+
+    fn get_value(&self) -> Self::Output {
+        self.value
+    }
+}
