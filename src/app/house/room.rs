501cc8bcdd96d9abeb6ef3a37391ec60f108a8f4
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2024-09-02 14:53:00 +0100

    added the hashmap for properties in the room

--- src/app/house/room.rs
+++ src/app/house/room.rs
@@ -3,4 +3,19 @@ mod processor;
 mod property;
 mod sensor;
 
-pub struct Room {}
+use crate::app::house::room::property::{
+    Humidity, Light, Property, Radiation, Smoke, Sound, Temperature, Vibration,
+};
+use std::collections::HashMap;
+
+pub struct Room {
+    properties: HashMap<String, Box<dyn Property>>,
+}
+
+impl Room {
+    pub fn new() -> Room {
+        Room {
+            properties: HashMap::new(),
+        }
+    }
+}
