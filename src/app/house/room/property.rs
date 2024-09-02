61b0dadb331fd646dc8a0f119db1fb8859600cbe
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2024-09-02 15:01:10 +0100

    added default trait to all structs in properties

--- src/app/house/room/property.rs
+++ src/app/house/room/property.rs
@@ -5,42 +5,49 @@ pub trait Property {
 
 //os sensores e aparelhos devem comunicar com as propriedades através das suas chaves
 
+#[derive(Default)]
 pub struct Temperature {
     value: u16,
 }
 
 impl Property for Temperature {}
 
+#[derive(Default)]
 pub struct Light {
     value: u16,
 }
 
 impl Property for Light {}
 
+#[derive(Default)]
 pub struct Radiation {
     value: u16,
 }
 
 impl Property for Radiation {}
 
+#[derive(Default)]
 pub struct Vibration {
     value: u16,
 }
 
 impl Property for Vibration {}
 
+#[derive(Default)]
 pub struct Humidity {
     value: u16,
 }
 
 impl Property for Humidity {}
 
+#[derive(Default)]
 pub struct Smoke {
     value: u16,
 }
 
 impl Property for Smoke {}
 
+#[derive(Default)]
 pub struct Sound {
     value: u16,
 }
