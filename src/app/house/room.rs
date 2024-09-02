2168a6ac53322ae7ab5c776e1094b1db88d3e3cd
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2024-09-02 15:15:39 +0100

    added the properties to the room

--- src/app/house/room.rs
+++ src/app/house/room.rs
@@ -14,8 +14,19 @@ pub struct Room {
 
 impl Room {
     pub fn new() -> Room {
-        Room {
-            properties: HashMap::new(),
-        }
+        let mut properties: HashMap<String, Box<dyn Property>> = HashMap::new();
+
+        properties.insert(
+            String::from("Temperature"),
+            Box::new(Temperature::default()),
+        );
+        properties.insert(String::from("Light"), Box::new(Light::default()));
+        properties.insert(String::from("Radiation"), Box::new(Radiation::default()));
+        properties.insert(String::from("Vibration"), Box::new(Vibration::default()));
+        properties.insert(String::from("Humidity"), Box::new(Humidity::default()));
+        properties.insert(String::from("Smoke"), Box::new(Smoke::default()));
+        properties.insert(String::from("Sound"), Box::new(Sound::default()));
+
+        Room { properties }
     }
 }
