98178011a2a088cf4d8962398049ac8906fe9c43
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2024-09-04 10:33:55 +0100

    adds rooms to House

--- src/app/house.rs
+++ src/app/house.rs
@@ -27,11 +27,57 @@ impl House {
         })
     }
 
-    pub fn add_room(&self) {
-        todo!()
+    pub fn add_room(&mut self, row: u8, column: u8) -> Result<(), &'static str> {
+        if row > self.height {
+            return Err("row is higher than allowed");
+        }
+        if column > self.width {
+            return Err("column is higher than allowed");
+        }
+
+        for room in &self.rooms {
+            if room.row() == row && room.column() == column {
+                return Err("a room is already occupying that position");
+            }
+        }
+
+        self.rooms.push(Room::new(row, column));
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::app::house::House;
+
+    #[test]
+    fn add_one_room() {
+        let mut house = House::build(2, 2).unwrap();
+
+        house.add_room(1, 1).unwrap();
+
+        assert_eq!(1, house.rooms.len());
     }
 
-    pub fn remove_room(&self) {
-        todo!()
+    #[test]
+    #[should_panic]
+    fn adds_one_room_on_top_another() {
+        let mut house = House::build(2, 2).unwrap();
+
+        house.add_room(1, 1).unwrap();
+        house.add_room(1, 1).unwrap();
+
+        assert_eq!(1, house.rooms.len());
+    }
+
+    #[test]
+    #[ignore]
+    //should be run alone
+    fn checks_rooms_id() {
+        let mut house = House::build(2, 2).unwrap();
+
+        house.add_room(1, 1).unwrap();
+
+        assert_eq!(String::from("r0"), house.rooms.get(0).unwrap().id());
     }
 }
